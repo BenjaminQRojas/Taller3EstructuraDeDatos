f88b4e59618aba19d384a3f3f8560a098d6bb8d0
Fabrich21 <146675966+Fabrich21@users.noreply.github.com>
2024-06-18 08:41:25 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 
+
+/* 
+leerArchivo
+*/
+
+/*InterfazUsuario
+   opciones : Ingresar, buscar(ID), modificar  (transacciones)
+   buscar historal de transacciones sospechosas
+   generar Reportes
+*/
 int main()
 {
+    //InterfazUsuario()
     std::cout<<"Hello World"<<std::endl;
 
     return 0;
